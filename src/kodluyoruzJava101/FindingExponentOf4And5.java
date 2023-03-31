047ca4c276442bffede654c628cdb28beed37d5c
Kaan Danisment <kaandanisment1@gmail.com>
2023-03-31 19:29:28 +0300

    Practice added

--- src/kodluyoruzJava101/FindingExponentOf4And5.java
+++ src/kodluyoruzJava101/FindingExponentOf4And5.java
@@ -6,21 +6,15 @@ public class FindingExponentOf4And5 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter a number: ");
-        int n = scan.nextInt(), x = 1, y = 1;
+        int n = scan.nextInt(), p = 0;
 
-        for (int i = 0; i < n; i++){
-            while (i==0){
-                System.out.println(i + "th power of 4: 1");
-                System.out.println(i + "th power of 5: 1" );
-                i++;
+        for (int i = 1, j = 1; i <= n || j <= n; i*=4, j*=5){
+            if (i < n){
+                System.out.println(p + "th power of 4: " + i);
             }
-            if (x <= n){
-                x *= 4;
-                System.out.println(i + "th power of 4: " + x);
-            }
-            if (y <= n){
-                y *= 5;
-                System.out.println(i + "th power of 5: " + y);
+            if (j < n){
+                System.out.println(p + "th power of 5: " + j);
+                p++;
             }
         }
     }
