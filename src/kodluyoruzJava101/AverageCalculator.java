7af6baaa3c86afd36542554e60eaa949b2d99c2c
Kaan Danisment <kaandanisment1@gmail.com>
2023-03-31 11:19:20 +0300

    Practice added

--- src/kodluyoruzJava101/AverageCalculator.java
+++ src/kodluyoruzJava101/AverageCalculator.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class AverageCalculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a number: ");
         int k = scan.nextInt(), t = 0, c = 0;
 
         for (int i = 1; i <= k; ++i){
@@ -13,6 +14,6 @@ public class AverageCalculator {
                 c ++;
             }
         }
-        System.out.println("Average: " + (t/c));
+        System.out.println("Average of number dividing by 3 and 4 without remainder: " + (t/c));
     }
 }
